b86eaf119e4b65f4301ac10ccc503e7c6532641e
Manoj Aryal <manojaryal1010@gmail.com>
2026-07-21 10:48:48 +0545

    Clear repository

--- special_token.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-encoded_input = tokenizer("How are you?")
-print(encoded_input["input_ids"])
-tokenizer.decode(encoded_input["input_ids"])
